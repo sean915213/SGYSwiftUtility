9be205f5bde1740c8897936306edd2321156d7e6
Sean G. Young <sean.g.young@gmail.com>
2016-04-23 17:19:10 -0400

    iOS: Async operation changes.

--- Pod/Classes/Operations/ConcurrentOperation.swift
+++ Pod/Classes/Operations/ConcurrentOperation.swift
@@ -1,49 +1,40 @@
 //
-//  ConcurrentOperation.swift
+//  AsyncOperation.swift
 //
 //  Created by Sean G Young on 3/30/16.
 //  Copyright © 2016 Sean G Young. All rights reserved.
 
 import Foundation
 
-public class ConcurrentOperation: SGYOperation {
+public class AsyncOperation: SGYOperation {
     
     // MARK: Required NSOperation properties to support concurrent execution
     // NOTE: 'isExecuting' and 'isFinished' key-value notifications are required so ObjC classes can observe properties.
     
-    // Indicates we do not finish when 'main' exits.  Does not matter when used in an NSOperationQueue (as this generally will be), but this declaration ensures this operation works as intended outside a queue.
+    // Indicates we do not finish when `main` exits.  Does not matter when used in an NSOperationQueue (as this generally will be), but this declaration ensures this operation works as intended outside a queue.
     public override var asynchronous: Bool { return true }
     
     private var _executing: Bool = false
-    public override var executing: Bool {
+    // NSOperation documentation indicates this property must be overrided and emmit KVO notifications. Marking dynamic emits a notification for `executing`.  It does not emit for `isExecuting`, which the documentation suggests is required.
+    // NOTE: In my testing the behavior of this class in an `NSOperationQueue` is not affected by the value of this property at all.
+    public override dynamic var executing: Bool {
         get { return _executing }
-        
-        set {
-            willChangeValueForKey("executing")
-            willChangeValueForKey("isExecuting")
-            _executing = newValue
-            didChangeValueForKey("isExecuting")
-            didChangeValueForKey("executing")
-        }
+        set { _executing = newValue }
     }
     
     private var _finished: Bool = false
-    public override var finished: Bool {
+    // NSOperation documentation indicates this property must be overrided and emmit KVO notifications. Marking dynamic emits a notification for "finished".  It does not emit for `isFinished`, which the documentation suggests is required.
+    // NOTE: In my testing the behavior of this class in an `NSOperationQueue` is not affected by KVO notifications at all.
+    // NOTE: In my testing this property alone completely determines whether dependent operations and `completionBlock:` are fired.  Also determines whether `NSOperationQueue` considers the operation completed and removes from queue.
+    public override dynamic var finished: Bool {
         get { return _finished }
-        
-        set {
-            willChangeValueForKey("finished")
-            willChangeValueForKey("isFinished")
-            _finished = newValue
-            didChangeValueForKey("isFinished")
-            didChangeValueForKey("finished")
-        }
+        set { _finished = newValue }
     }
     
     // MARK: Methods
     
     public override func start() {
-        // CANCEL CHECK
+        // Cancel check
         guard !cancelled else {
             endExecution()
             return
