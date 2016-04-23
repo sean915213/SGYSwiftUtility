944c3c2bcf25ac627c0e477ba32f96d49f59f6b8
Sean G. Young <sean.g.young@gmail.com>
2016-04-23 17:26:06 -0400

    Minor changes.

--- Pod/Classes/Operations/AsyncOperation.swift
+++ Pod/Classes/Operations/AsyncOperation.swift
@@ -8,14 +8,13 @@ import Foundation
 
 public class AsyncOperation: SGYOperation {
     
-    // MARK: Required NSOperation properties to support concurrent execution
-    // NOTE: 'isExecuting' and 'isFinished' key-value notifications are required so ObjC classes can observe properties.
-    
+    // MARK: Required NSOperation properties to support asynchronous execution
+
     // Indicates we do not finish when `main` exits.  Does not matter when used in an NSOperationQueue (as this generally will be), but this declaration ensures this operation works as intended outside a queue.
     public override var asynchronous: Bool { return true }
     
     private var _executing: Bool = false
-    // NSOperation documentation indicates this property must be overrided and emmit KVO notifications. Marking dynamic emits a notification for `executing`.  It does not emit for `isExecuting`, which the documentation suggests is required.
+    // `NSOperation` documentation indicates this property must be overrided and emmit KVO notifications. Marking dynamic emits a notification for `executing`.  It does not emit for `isExecuting`, which the documentation suggests is required.
     // NOTE: In my testing the behavior of this class in an `NSOperationQueue` is not affected by the value of this property at all.
     public override dynamic var executing: Bool {
         get { return _executing }
@@ -23,7 +22,7 @@ public class AsyncOperation: SGYOperation {
     }
     
     private var _finished: Bool = false
-    // NSOperation documentation indicates this property must be overrided and emmit KVO notifications. Marking dynamic emits a notification for "finished".  It does not emit for `isFinished`, which the documentation suggests is required.
+    // `NSOperation` documentation indicates this property must be overrided and emmit KVO notifications. Marking dynamic emits a notification for "finished".  It does not emit for `isFinished`, which the documentation suggests is required.
     // NOTE: In my testing the behavior of this class in an `NSOperationQueue` is not affected by KVO notifications at all.
     // NOTE: In my testing this property alone completely determines whether dependent operations and `completionBlock:` are fired.  Also determines whether `NSOperationQueue` considers the operation completed and removes from queue.
     public override dynamic var finished: Bool {
