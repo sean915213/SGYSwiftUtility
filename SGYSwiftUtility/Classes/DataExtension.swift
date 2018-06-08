a53e006c016f305703961479a93ce44151765bcf
sdot <sean.g.young@gmail.com>
2018-06-08 18:22:55 -0400

    iOS: Added Codable items and updated to Swift 4.2 + Xcode 10 project settings.

--- SGYSwiftUtility/Classes/DataExtension.swift
+++ SGYSwiftUtility/Classes/DataExtension.swift
@@ -14,7 +14,7 @@ extension Data {
         // Make sure this string CAN be converted (ie. has even number of characters)
         guard !string.isEmpty && string.lengthOfBytes(using: String.Encoding.utf8) % 2 == 0 else { return nil }
         // Array to store bytes
-        var byteArray = Array<UInt8>()
+        var byteArray = [UInt8]()
         // Grab the bytes from the string in 2 character chunks
         var index = string.startIndex
         repeat {
@@ -30,30 +30,25 @@ extension Data {
         self.init(bytes: byteArray)
     }
     
-    public func writeToTempFile(_ fileName: String) -> URL? {
+    public func writeToTempFile(named name: String) throws -> URL {
         let fileManager = FileManager.default
         // Create unique directory inside temp folder
         let tempPath = NSTemporaryDirectory()
-        do {
-            let containingDirectory = URL(fileURLWithPath: tempPath).appendingPathComponent(ProcessInfo.processInfo.globallyUniqueString)
-            try fileManager.createDirectory(at: containingDirectory, withIntermediateDirectories: false, attributes: nil)
-            // Sanitize fileName
-            var sanitizedFileName = fileName
-            sanitizedCharacters.forEach { sanitizedFileName = sanitizedFileName.replacingOccurrences(of: $0, with: "") }
-            // Create url in this directory
-            let dataUrl = containingDirectory.appendingPathComponent(sanitizedFileName)
-            // Finally write data
-            try write(to: dataUrl)
-            // Return url
-            return dataUrl
-        } catch let error as NSError {
-            NSLog("[NSDataExtension] Warning - writeToTempFile failed with error: \(error.localizedDescription).")
-            return nil
-        }
+        let containingDirectory = URL(fileURLWithPath: tempPath).appendingPathComponent(ProcessInfo.processInfo.globallyUniqueString)
+        try fileManager.createDirectory(at: containingDirectory, withIntermediateDirectories: false, attributes: nil)
+        // Sanitize fileName
+        var sanitizedFileName = name
+        sanitizedCharacters.forEach { sanitizedFileName = sanitizedFileName.replacingOccurrences(of: $0, with: "") }
+        // Create url in this directory
+        let dataUrl = containingDirectory.appendingPathComponent(sanitizedFileName)
+        // Finally write data
+        try write(to: dataUrl)
+        // Return url
+        return dataUrl
     }
     
     public func hexString() -> String? {
-        guard count > 0 else { return nil }
+        guard !isEmpty else { return nil }
         var hexString = String()
         // Append to string
         for byte in self { hexString += String(format: "%02x", UInt(byte)) }
