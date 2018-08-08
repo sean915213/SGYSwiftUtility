d672d643a73a143684207bb3c9625977f0c26862
sdot <sean.g.young@gmail.com>
2018-08-08 16:32:55 -0400

    Added NSEntityDescriptionExtension.

---
+++ SGYSwiftUtility/Classes/NSEntityDescriptionExtension.swift
@@ -0,0 +1,23 @@
+//
+//  NSEntityDescriptionExtension.swift
+//  SGYSwiftUtility
+//
+//  Created by Sean G Young on 8/8/18.
+//
+
+import Foundation
+import CoreData
+
+protocol NamedManagedObject where Self: NSManagedObject {
+    static var entityName: String { get }
+}
+
+extension NSEntityDescription {
+    class func insertNewObject<T>(forEntityClass entityClass: T.Type, into context: NSManagedObjectContext) -> T where T: NamedManagedObject {
+        return insertNewObject(forEntityName: entityClass.entityName, into: context) as! T
+    }
+}
+
+extension NamedManagedObject {
+    static var entityName: String { return String(describing: self) }
+}
