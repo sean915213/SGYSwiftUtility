1e2225d0f5406b8483d5b79cd1b59cc29e2dbc6f
sdot <sean.g.young@gmail.com>
2018-08-08 18:58:22 -0400

    Modified NSManagedObjectContext to make extended functions public.

--- SGYSwiftUtility/Classes/NSManagedObjectContextExtension.swift
+++ SGYSwiftUtility/Classes/NSManagedObjectContextExtension.swift
@@ -11,12 +11,12 @@ import CoreData
 
 extension NSManagedObjectContext {
     
-    func existingObject<T: NSManagedObject>(with objectID: NSManagedObjectID) throws -> T {
+    public func existingObject<T: NSManagedObject>(with objectID: NSManagedObjectID) throws -> T {
         // Force cast since object not found will throw and if an object is found of the wrong type the caller messed up.
         return try existingObject(with: objectID) as! T
     }
     
-    func fetch<T: NSManagedObject>(_ request: NSFetchRequest<T>) throws -> T? {
+    public func fetch<T: NSManagedObject>(_ request: NSFetchRequest<T>) throws -> T? {
         // We only expect one entry so limit request
         request.fetchLimit = 1
         return try fetch(request).first
